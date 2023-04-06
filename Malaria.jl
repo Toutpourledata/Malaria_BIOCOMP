d5008f235c7ebf2b48706284a7beb374e6132876
CocoBongo26 <129393137+CocoBongo26@users.noreply.github.com>
2023-04-06 10:05:29 -0400

    Co-authored-by: Amélie Guesdon <Amelie21-01@users.noreply.github.com> Co-authored-by: Toutpourledata <Toutpourledata@users.noreply.github.com> Co-authored-by: SabrinaSTP <SabrinaSTP@users.noreply.github.com> Co-authored-by: eq41635 <eq41635@users.noreply.github.com>

--- Malaria.jl
+++ Malaria.jl
@@ -1,15 +1,20 @@
+# Chargement des packages
 using ModelingToolkit
 using CairoMakie
 using Latexify
 using DifferentialEquations
 
-# Paramètres et variables
-
+# Ajout des paramètres et variables
 @parameters t ϕ Z ph qv h ψh ψv μh λ β b δ θ
+#
 @variables Hs(t) Hp(t) Hr(t) Vs(t) Vp(t)
+# H : Humains
+# V : Vecteurs
+# s : Individus susceptibles
+# p : Individus parasités
+# r : Individus immunisés
 
-
-# Système d'équations différentielles
+# système d'équations différentielles 
 D = Differential(t)
 Malaria_equations = [
 
@@ -19,48 +24,43 @@ Malaria_equations = [
     D(Vp) ~ λ*Hp*Vs + Z - Vs*ψv,
     D(Vs) ~ Z-λ*Vp*Hs
 ]
+#valeurs des paramètres
+param = [ϕ => 0.05,  #Entrants dans Hs (naissance et immigration)
+        Z => 1,      #Entrants (vecteur)
+        ph => 0.001, #Probabilité qu'un immigrant soit parasité?
+        qv => 0.05,  #
+        h => 0.01,   #
+        ψh => 0.08,  #Taux de sortie des humains
+        ψv => 0.08,  #Taux de sortie des vecteurs
+        μh => 0.05,  #Taux de mortalité dû au parasite
+        λ => 0.075,  #Taux de transmission
+        β => 0.02,   #Taux de guérison
+        b => 0.015,  #
+        δ => 0.025,  #Taux de perte d'immunité
+        θ => 0.02]   #Taux de vaccination
 
-param = [ϕ => 0.05, 
-        Z => 1,
-        ph => 0.001,
-        qv => 0.05,
-        h => 0.01,
-        ψh => 0.08,
-        ψv => 0.08,
-        μh => 0.05,
-        λ => 0.075,
-        β => 0.02,
-        b => 0.015,
-        δ => 0.025,
-        θ => 0.02]
-# initial conditions
-
+# conditions initiales
 u0 = [
-    Hs => 5000,
-    Hp => 1000,
-    Hr => 500,
-    Vs => 10000,
-    Vp => 500]
+    Hs => 5000,   # humains susceptibles
+    Hp => 1000,   # humains parasités
+    Hr => 500,    # humains rétablis
+    Vs => 10000,  # vecteur susceptibles
+    Vp => 500]    # vecteur parasités
 
-# Durée
+# durée
 duree = (0.0, 100)
 
-# Solve the system of differential equations
-#Latexify.latexify(Malaria_equations) |> render
-
+# résoudre le système d'équations différentielles
 @named Malaria_system = ODESystem(Malaria_equations)
 
 prob = ODEProblem(Malaria_system, u0, duree, param, jac=true)
 sol = solve(prob, saveat=0.0:0.5:100, verbose=true)
 
-#plt = plot(sol.t, Float32.(sol[end]), vars=[Hs, Hp, Hr, Vs, Vp], legend=:right, xlabel="Time (years)", ylabel="Population size")
-#display(plt)
-
-# Figure
+# graphique
 fig = Figure(; resolution=(1000,500))
 timecourse = Axis(fig[1,1]; xlabel="Temps", ylabel="Population")
 
-# Add lines for each variable to the plot
+# on ajoute une ligne pour chaque variable
 lines!(timecourse, sol[1, :], label="Hs", color=:black)
 lines!(timecourse, sol[2, :], label="Hp", color=:red)
 lines!(timecourse, sol[3, :], label="Hr", color=:green)
@@ -69,4 +69,5 @@ lines!(timecourse, sol[5, :], label="Vp", color=:orange)
 xlims!(timecourse, (0., 100.))
 ylims!(timecourse, (0., 10000.))
 
+#générer le graphique
 current_figure()
